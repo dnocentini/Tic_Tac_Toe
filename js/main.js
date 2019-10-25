39013e6bf704f793582d5b48227998056d86bb0b
Dinny Nocentini <dinny.nocentini@gmail.com>
2019-10-25 17:37:16 -0500

    function click done.

--- js/main.js
+++ js/main.js
@@ -1,45 +1,49 @@
 /*playerOne wiil be "X" and playerTwo will be "O"*/
-/*playerOne will click in a square and "X" will appears inside the square clicked*/
+/*playerOne will 'click' in a square and "X" will appears inside the square clicked*/
 /*return*/
 /*anotherPlayerTurns*/
-/*playerTwo will click in a square and "X" will appears inside the square clicked*/
+/*playerTwo will 'click' in a square and "X" will appears inside the square clicked*/
 /*return*/
 /*anotherPlayerTurns*/
 /*the game will not return if a player does one of the winCombinations*/
-/*the "winCombinations" is basically whe the first player to get 3 of he/her marks in a row (up, down, across, or diagonally) is the winner.*/
+/*the "winCombinations" is basically when the first player to get 3 of he/her marks in a row (up, down, across, or diagonally) is the winner.*/
 /*when all 9 squares are full, the game is over.*/
 
 
-
-// 1) Define required constants
+/*----- constants -----*/
 // const playerOne = "X"
 // const playerTwo = "O"
-
 const x = 1
 const o = -1
 const WinCombinations = [
-    [sq0, sq1, sq2],
-    [sq3, sq4, sq ]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8 ],
+    []
 
 ]
 
-// 2) Define required variables used to track the state of the game
+/*----- app's state (variables) -----*/
+let gameBoardEl = document.getElementById ('game-board')
+
 let sqClicked
 
-// 3) Store elements on the page that will be accessed in code more than once in variables to make code more concise, readable and performant.
-
-// 4) Upon loading the app should:
-// 	4.1) Initialize the state variables
-// 	4.2) Render those values to the page
-// 	4.3) Wait for the user to click a square
-
-// 5) Handle a player clicking a square
+/*----- cached element references -----*/
 
 
+/*----- event listeners -----*/
+gameBoardEl.addEventListener('click', handButtonClick);
+}    
 
-// 6) Handle a player clicking the replay button
+
+/*----- functions -----*/
+
+function handButtonClick (evt) {
+    console.log(evt.target.id); 
 
 
+
+    
 
 // document.querySelectorAll('evt')
 //     .addEventListener('click', handButtonClick)
@@ -47,6 +51,4 @@ let sqClicked
 // document.getElementById('resetGame')
 //     .addEventListener('click', handButtonClick)
 
-document.getElementsByTagName('table')
-    .addEventListener('click', handButtonClick)    
-    console.log(document.getElementsByTagName('table');
+
